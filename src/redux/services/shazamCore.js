2391a8b759e22c2a171d5334d4b155c5fcced793
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-02 20:56:08 +0700

    add .enc

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,7 +4,7 @@ export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({baseUrl:'https://shazam-core.p.rapidapi.com/v1/',
     prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', RAPID_API_KEY);
+        headers.set('X-RapidAPI-Key', process.env.RAPID_API_KEY);
 
         return headers;
         },
