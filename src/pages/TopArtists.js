91c7c379ee634bc129eb08db5923b55b4a986bf2
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-17 22:25:38 +0700

    Add TopArtists

--- src/pages/TopArtists.js
+++ src/pages/TopArtists.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const TopArtists = () => {
     return(
-        <div>TopArtists</div>
+        <div className=" flex flex-col">
+            <div className=" w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className=" font-bold text-3xl text-white text-left ml-10">Discover</h2>
+            </div>
+            <div className=" text-white flex flex-wrap justify-center gap-8 ">
+                <p className="text-white"> hello</p>
+            </div>
+        </div>
     )
+    
 }
 
 export default TopArtists;
