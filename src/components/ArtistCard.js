5b6eff96d8a138f9a47829304b98069801f078af
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-22 22:27:22 +0700

    Add ArtistCard

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,8 +1,22 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ArtistCard = () => {
+const ArtistCard = ({track}) => {
+    const navigate = useNavigate()
+
     return(
-        <div>ArtistCard</div>
+        <div className=' flex flex-col w-[250px] p-4 bg-white bg-opacity-20  backdrop-blur-sm rounded-lg cursor-pointer select-none'
+            onClick={()=> navigate(`/artists/${track?.artists[0].adamid}`)}
+        >
+            <div className='relative w-full h-56 group'>
+                <img className=' w-full h-full rounded-lg' src={track?.images?.coverart} alt='track_img'/>
+            </div>
+            <div className=' mt-4 flex flex-col'>
+                <p className=' text-ls font-semibold text-gray-300 mt-1'>
+                        {track.subtitle}
+                </p>
+            </div>
+
+        </div>
     )
 }
 
