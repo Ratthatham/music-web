b9235c730cf7834a16005759cb0f11af5d404802
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-22 14:24:33 +0700

    add SongBar

--- src/components/SongBar.js
+++ src/components/SongBar.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-const SongBar = () => {
-    return(
-        <div>SongBar</div>
+
+const SongBar = ({song, i}) => {
+    
+    return (
+        <div className=" w-full flex flex-row items-center my-2" >
+                <h3 className=" font-bold text-base text-white mr-3">{`${i+1}.`}</h3>
+                <div className="flex-1 flex flex-row justify-between items-center">
+                    <img src={song?.images?.coverart} alt={song.title}  className=' w-20 h-20 rounded-lg'/>
+                </div>
+                <div className="flex-1 flex flex-col justify-center mx-3">
+                    <p className=" text-xl font-bold text-white">{song.title}</p>
+                    <p className=" text-base text-gray-300 mt-1">{song.subtitle}</p>
+                </div>
+        </div>
     )
-}
+    }
+
+
 
 export default SongBar;
