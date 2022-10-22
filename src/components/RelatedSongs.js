b0a1441adede1b6d9e0750c7c922d55c986c1f77
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-22 14:23:21 +0700

    Add RelatedSongs

--- src/components/RelatedSongs.js
+++ src/components/RelatedSongs.js
@@ -1,8 +1,28 @@
 import React from "react";
+import SongBar from "./SongBar";
 
-const RelatedSongs = () => {
+const RelatedSongs = ({data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId}) => {
     return(
-        <div>RelatedSongs</div>
+        <div className=" flex flex-col ml-10">
+            <h1 className=" font-bold text-2xl text-white "> RelatedSongs</h1>
+            <div className="mt-6 w-full flex flex-col">
+                {
+                    data?.map((song, i)=>(
+                        <SongBar
+                            key={`${song.key}-${artistId}`}
+                            song={song}
+                            i = {i}
+                            artistId={artistId}
+                            isPlaying={isPlaying}
+                            activeSong={activeSong}
+                            handlePauseClick={handlePauseClick}
+                            handlePlayClick={handlePlayClick}
+                        />
+                    ))
+                }
+            </div>
+
+        </div>
     )
 }
 
