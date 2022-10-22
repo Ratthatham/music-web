1fb04eda1b4fe3e360a6071598f634be6dada7eb
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-22 21:41:13 +0700

    Add TopPlay

--- src/components/TopPlay.js
+++ src/components/TopPlay.js
@@ -1,58 +1,18 @@
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import {useGetTopChartsQuery} from '../redux/services/shazamCore'
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
-import PlayPause from "./PlayPause";
-
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css';
 import { FreeMode } from 'swiper';
+import ChartCard from "./ChartCard";
 
 
 
 const TopPlay = () => {
-    const dispatch = useDispatch();
     const {isPlaying, activeSong} = useSelector((state)=>state.player)
     const {data, isFetching, isError} = useGetTopChartsQuery();
     const topPlayers = data?.slice(0,5);
     const topTenArtists = data?.slice(0,10);
-
-
-    
-    const TopChartCard = ({song, i, isPlaying, activeSong, data}) => {
-        
-        const handlePauseClick = () => {
-            dispatch(playPause(false))
-        }
-    
-        const handlePlayClick = () => {
-            dispatch(setActiveSong({song, data, i}));
-            dispatch(playPause(true))
-        }
-
-        return (
-            <div className=" w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2">
-                    <h3 className=" font-bold text-base text-white mr-3">{`${i+1}.`}</h3>
-                    <div className="flex-1 flex flex-row justify-between items-center">
-                        <img src={song?.images?.coverart} alt={song.title}  className=' w-20 h-20 rounded-lg'/>
-                    </div>
-                    <div className="flex-1 flex flex-col justify-center mx-3">
-                        <Link to={`/songs/${song.key}`}>
-                            <p className=" text-xl font-bold text-white">{song.title}</p>
-                        </Link>
-                        <p className=" text-base text-gray-300 mt-1">{song.subtitle}</p>
-                    </div>
-                    <PlayPause 
-                        song={song}
-                        handlePlayClick={handlePlayClick}
-                        handlePauseClick={handlePauseClick}
-                        isPlaying={isPlaying}
-                        activeSong={activeSong}
-                        size={30}
-                    />
-            </div>
-        )
-        }
     
     return (
         <div className=" xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
@@ -67,7 +27,7 @@ const TopPlay = () => {
                 <div className=" mt-4 flex flex-col gap-1">
                     {
                         topPlayers?.map((song, i)=>(
-                            <TopChartCard 
+                            <ChartCard 
                                 key={song.key}
                                 song={song}
                                 i={i}
