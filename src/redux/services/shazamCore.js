24a1cbdd9616348edc07949c95258354ec28730c
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-22 13:48:03 +0700

    Edit shazamCore

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,18 +4,23 @@ export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({baseUrl:'https://shazam-core.p.rapidapi.com/v1/',
     prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', 'RAPID_API');
-
+        headers.set('X-RapidAPI-Key', 'c735dba039msh966a299e7017b9fp17ed06jsn28c008b6b82c');
         return headers;
         },
 
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world'}),
+        getSongsDetails: builder.query({query: (songid) => `/tracks/details?track_id=${songid}`}),
+        getSongRelated: builder.query({query: (songid) => `/tracks/related?track_id=${songid}`})
     })
 
 })
 
-export const {useGetTopChartsQuery} = shazamCoreApi;
+export const {
+    useGetTopChartsQuery, 
+    useGetSongsDetailsQuery,
+    useGetSongRelatedQuery
+} = shazamCoreApi;
 
 
