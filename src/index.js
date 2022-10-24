b55ef92f51aee86636c4015975f2cef9ebadbb48
Ratthatham <ratthatham.siridol@gmail.com>
2022-10-24 23:28:17 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 import {Provider} from 'react-redux'
 import store from '../src/redux/store';
@@ -12,11 +12,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router >
+    <HashRouter >
       <Provider store = {store}>
         <App />
       </Provider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
 
